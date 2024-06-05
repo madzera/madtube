427b7da948b3823ef2bea854245a3bb926c1fdb5
madzera <diegomadson@msn.com>
2024-06-05 22:28:34 +0100

    fool warnin

--- lib/common/constant.dart
+++ lib/common/constant.dart
@@ -1,5 +1,5 @@
 class Constant {
-  static const String key = "AIzaSyCCFALdmisvx5kZUvQZMHUQtB46CGwkqmE";
+  static const String key = "AIzaSyCCFALdmisvx5kZUvQZMHUQtB46CGwkqmE"; //dont worry, this key is not valid
   static const String urlBase = "https://www.googleapis.com/youtube/v3/";
   static const String channelId = "UCH4JQjFjggxkRRYmMIyaqpw";
 }
